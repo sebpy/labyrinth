be65e97b093c6b14d48fc01435c37fed47507408
sebpy <sebastien.fantinati@gmail.com>
2019-01-27 16:36:56 +0100

    Add show and move in laby.py for test. Game is functionnel

--- start.py
+++ start.py
@@ -5,12 +5,9 @@ import laby
 #import mg_character
 #import items
 
-def move():
-	""" Move the character with input keys"""
-
-	key = input("Appuis sur une touche")
-	print(key)
 
 if __name__ == "__main__":
-	laby.loadLabyrinth()
-	move()
+
+	lab = laby.Labyrinth()
+	lab.load_map()
+	lab.dislay_map()
